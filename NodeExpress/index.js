574cd0f2af1bee1d6e451e126b0f3ca41df59b6b
sari-swiatycki <s0534160286@gmail.com>
2025-02-06 18:07:18 +0200

    updates

--- NodeExpress/index.js
+++ NodeExpress/index.js
@@ -35,3 +35,12 @@ app.get("/", async (req, res) => {
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
+
+
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');  // או הצהרה ספציפית למקור
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
